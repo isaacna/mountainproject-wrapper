e94663ee4027cc07506766216bc4114a8df49ebd
isaac219 <isaac219@gmail.com>
2018-05-02 16:10:11 -0500

    changed string concatenation to use urlparse and requests

--- wrapper/mp.py
+++ wrapper/mp.py
@@ -1,4 +1,4 @@
-import requests
+import requests, urlparse
 
 class MP(object):
     #init with member variables
@@ -9,37 +9,29 @@ class MP(object):
 
     #create getUser endpoint and get JSON 
     def get_user(self):
-        url = self.base_url + 'get-user?'
-        url += ('email=' + self.email)
-        url += ('&key=' + self.key)
-        r = requests.get(url)
+        url = urlparse.urljoin(self.base_url,'get-user')
+        vals = {'email' : self.email, 'key' : self.key} 
+        r = requests.get(url, params=vals)
         return r.json()
     
     #create getTicks endpoint and get JSON
     def get_ticks(self):
-        url = self.base_url + 'get-ticks?'
-        url += ('email=' + self.email)
-        url += ('&key=' + self.key)
-        r = requests.get(url)
+        url = urlparse.urljoin(self.base_url,'get-ticks')
+        vals = {'email' : self.email, 'key' : self.key} 
+        r = requests.get(url, params=vals)
         return r.json()
 
     #create getToDos endpoint and get JSON
-    def get_to_dos(self,start_index=0):
-        url = self.base_url + 'get-to-dos?'
-        url += ('email=' + self.email)
-        url += ('&key=' + self.key)
-        url += ('&startPos=' + str(start_index))
-        r = requests.get(url)
+    def get_to_dos(self, start_index=0):
+        url = urlparse.urljoin(self.base_url,'get-to-dos')
+        vals = {'email' : self.email, 'key' : self.key, 'startPos' : start_index} 
+        r = requests.get(url, params=vals)
         return r.json()
 
     #create getRoutesLatLon endpoint and get JSON
     def get_routes_lat_lon(self, lat, lon, limit=15, dist=10):
-        url = self.base_url + 'get-routes-for-lat-lon?'
-        url += ('lat=' + str(lat))
-        url += ('&lon=' + str(lon))
-        url += ('&key=' + self.key)
-        url += ('&maxResults=' + str(limit))
-        url += ('&maxDistance=' + str(dist))
-        r = requests.get(url)
+        url = urlparse.urljoin(self.base_url,'get-routes-for-lat-lon')
+        vals = {'lat' : lat, 'lon' : lon, 'key' : self.key, 'maxResults' : limit, 'maxDistance' : dist} 
+        r = requests.get(url, params=vals)
         return r.json()
 
